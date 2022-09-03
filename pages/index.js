2f194c97401223a0199e8c36cb6b3c3153d58ca8
Anugrah Kresnaya <anugrahkresnaya.ak@gmail.com>
2022-09-03 12:27:40 +0700

    Revert "add navbar and eslint"

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,7 @@
-import Navbar from "../components/navbar/navbar";
-
 export default function Home() {
   return (
-    <div>
-      <Navbar />
-      <div className="container mx-auto px-4 flex items-center h-screen">
-        <div>
-          <h1 className="text-4xl mb-10">Hello,</h1>
-          <h1 className="text-8xl">I'm Anugrah! 👋</h1>
-          <p className="text-xl mt-10">
-            An Informatics Engineering Student who passionate in technology and web development<br/>especially Frontend engineering using React & NextJS
-          </p>
-        </div>
-      </div>
-    </div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   )
 }
